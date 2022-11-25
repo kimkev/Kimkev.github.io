b371a6847b883587617a523ce4ae1681882e8a5a
kevin <k6yk07@gmail.com>
2022-11-24 22:25:57 -0500

    create basic menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Blogs from "./components/Blogs";
+import Contact from "./components/Contact";
+import NoPage from "./components/NoPage";
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<NavBar />}>
           <Route index element={<Home />} />
+          <Route path="Home" element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
