382370ad8809e9ea87086dec1f44795c1f36d8e5
kevin <k6yk07@gmail.com>
2022-11-24 22:26:30 -0500

    create basic menu & rename directories

---
+++ src/components/NavBar.js
@@ -0,0 +1,47 @@
+import { Outlet, Link } from "react-router-dom";
+import '../styles/NavBar.css'
+
+const NavBar = () => {
+  return (
+    <>
+      <nav className='navigation'>
+      <Link to="/Home" className="brand-name">
+        Kevin Kim
+      </Link>
+      <button className="hamburger">
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className="navigation-menu">
+        <ul>
+          <li>
+            <Link to="/Home">Home</Link>
+          </li>
+          <li>
+          <Link to="/Blogs">Blogs</Link>
+          </li>
+          <li>
+          <Link to="/Contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+export default NavBar;
