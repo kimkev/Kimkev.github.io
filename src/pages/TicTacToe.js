96daf6b3383ad7a54d86aca21e5dc0a592bbe19b
kevin <k6yk07@gmail.com>
2023-02-06 21:11:48 -0500

    fix layout + reset button for tictactoe

--- src/pages/TicTacToe.js
+++ src/pages/TicTacToe.js
@@ -1,53 +1,65 @@
 import React, { useState } from "react";
 
 const TicTacToe = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [currentPlayer, setCurrentPlayer] = useState("X");
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [currentPlayer, setCurrentPlayer] = useState("X");
 
-  const handleClick = (index) => {
-    const newBoard = [...board];
-    if (newBoard[index] === null) {
-      newBoard[index] = currentPlayer;
-      setBoard(newBoard);
-      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
-    }
-  };
+    const handleClick = (index) => {
+        const newBoard = [...board];
+        if (newBoard[index] === null) {
+            newBoard[index] = currentPlayer;
+            setBoard(newBoard);
+            setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+        }
+    };
 
-  const renderSquare = (index) => {
+    const resetBoard = () => {
+        setBoard(Array(9).fill(null));
+        setCurrentPlayer("X");
+    };
+
+    const renderSquare = (index) => {
+        return (
+            <div
+                key={index}
+                style={{
+                    width: "100px",
+                    height: "100px",
+                    border: "1px solid black",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: "50px",
+                    fontWeight: "bold",
+                }}
+                onClick={() => handleClick(index)}
+            >
+                {board[index]}
+            </div>
+        );
+    };
+
     return (
-      <div
-        style={{
-          width: "100px",
-          height: "100px",
-          border: "1px solid black",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontSize: "50px",
-          fontWeight: "bold",
-        }}
-        onClick={() => handleClick(index)}
-      >
-        {board[index]}
-      </div>
+        <div>
+            <div
+                style={{
+                    display: "grid",
+                    gridTemplateColumns: "repeat(3, 100px)",
+                    gridTemplateRows: "repeat(3, 100px)",
+                    width: "300px",
+                    height: "300px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    margin: "auto",
+                }}
+            >
+                {board.map((square, index) => renderSquare(index))}
+            </div>
+            <button onClick={resetBoard} style={{ marginTop: "20px" }}>
+                Reset
+            </button>
+        </div>
     );
-  };
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        width: "300px",
-        height: "300px",
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "auto",
-      }}
-    >
-      {board.map((square, index) => renderSquare(index))}
-    </div>
-  );
 };
 
 export default TicTacToe;
